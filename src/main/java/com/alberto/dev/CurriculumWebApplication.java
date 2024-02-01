9e4048cf01c4173bfd318f7c750f0839800c7544
Alberto Dev <reynaga230@gmail.com>
2024-01-31 19:36:41 -0600

    root

---
+++ src/main/java/com/alberto/dev/CurriculumWebApplication.java
@@ -0,0 +1,13 @@
+package com.alberto.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurriculumWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alberto/dev/CurriculumWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
